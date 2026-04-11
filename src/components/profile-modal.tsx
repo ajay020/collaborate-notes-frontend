9ded856af06e56287aaf42e4a30388c5a5927a8a
ajay <ajayjarwal44@gmail.com>
2026-04-11 17:22:06 +0530

    fix: add icons to the items in profile menu

--- src/components/profile-modal.tsx
+++ src/components/profile-modal.tsx
@@ -1,40 +1,37 @@
 import { useAuthStore } from "@/store/auth-store"
+import { LogOutIcon, Trash } from "lucide-react"
 
 function ProfileModal({ onClose }: { onClose: () => void }) {
     const logout = useAuthStore(state => state.logout)
 
     return (
-        <div className="absolute bottom-16 left-4 w-56 bg-white border rounded shadow-lg">
+        <div className="absolute bottom-16 left-4 w-50 bg-white border rounded shadow-lg">
             <div className="bg-white rounded-lg p-4 w-80 shadow-lg">
                 <h2 className="text-lg font-semibold mb-4">Account</h2>
 
                 <button
-                    className="w-full text-left p-2 hover:bg-gray-100 rounded"
-                    onClick={() => alert("Update user")}
-                >
-                    Update Profile
-                </button>
-
-                <button
-                    className="w-full text-left p-2 hover:bg-gray-100 rounded"
+                    className="w-full flex items-center text-left p-2 hover:bg-gray-100 rounded"
                     onClick={() => alert("Delete user")}
                 >
-                    Delete Account
+                    <Trash size={14} className="mr-2" /> Delete Account 
                 </button>
 
                 <button
-                    className="w-full text-left p-2 hover:bg-red-100 text-red-600 rounded mt-2"
+                    className="w-full flex items-center text-left p-2 hover:bg-red-100 text-red-600 rounded mt-2"
                     onClick={logout}
                 >
-                    Logout
+                    <LogOutIcon size={14} className="mr-2" /> Logout 
                 </button>
 
-                <button
-                    className="mt-4 text-sm text-gray-500"
-                    onClick={onClose}
-                >
-                    Close
-                </button>
+                <div className=" flex justify-end">
+                    <button
+                        className="mt-4  px-3 py-2 text-sm text-gray-500  hover:text-black
+                    border hover:border-black rounded-full"
+                        onClick={onClose}
+                    >
+                        Close
+                    </button>
+                </div>
             </div>
         </div>
     )
